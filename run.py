1171f1b55be18257f101af84bc6275af877cd1d5
Gwgjnr <geoffwgg@gmail.com>
2021-09-09 17:32:16 +0000

    added basic defs for player and computer turns

--- run.py
+++ run.py
@@ -11,20 +11,38 @@ def startGame():
 
 
 def drawBoard():
-    print(f' {board[0]}| {board[1]}| {board[2]}')
-    print("--+--+--")
-    print(f' {board[3]}| {board[4]}| {board[5]}')
-    print("--+--+--")
-    print(f' {board[6]}| {board[7]}| {board[8]}\n')
+    print(f'{board[0]}|{board[1]}|{board[2]}')
+    print("-+-+-")
+    print(f'{board[3]}|{board[4]}|{board[5]}')
+    print("-+-+-")
+    print(f'{board[6]}|{board[7]}|{board[8]}\n')
 
 
 def randomFirstPlayer():
     firstPlayer = random.randint(1, 2)
     if firstPlayer == 1:
-        print(f'{name} will go first')
+        print(f'{name} will go first\n')
+        playerTurn()
     elif firstPlayer == 2:
-        print('Computer will go first')
+        print('Computer will go first\n')
+        computerTurn()
 
+
+def playerTurn():
+    selection = int(input("Please select your position using your numpad: "))
+    board[selection] = "X"
+    drawBoard()
+    computerTurn()
+
+
+def computerTurn():
+    print('Computers turn...\n')
+    print('.....\n')
+    print('...\n')
+    selection = random.randint(0, 8)
+    board[selection] = "O"
+    drawBoard()
+    playerTurn()
 # def for deciding who goes first
 # ask for position (add exceptions for invalid input)
 # draw computor position
