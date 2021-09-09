7ebbfd71d8e5e7489fee49339cc49312b4446471
Gwgjnr <geoffwgg@gmail.com>
2021-09-09 15:51:40 +0000

    created game def and added initial welcome and name input

--- run.py
+++ run.py
@@ -1,5 +1,10 @@
 # import random function for game
 # ask for name
+
+def game():
+    print('welcome to TICTACTOE!')
+    name = input("Please enter your name: ")
+    print(f'Thanks, {name}')
 # ask if you want to be x or o
 # def for deciding who goes first
 # ask for position (add exceptions for invalid input)
@@ -8,3 +13,5 @@
 # add elif for win conditions
 # add def to check if its a draw if board is full
 # declare winner and ask if they wany to play again
+
+game()
