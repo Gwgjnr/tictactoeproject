9328912962de5a536440950771035773489dc382
Gwgjnr <geoffwgg@gmail.com>
2021-09-09 17:02:02 +0000

    added def for randomly selecting who goes first

--- run.py
+++ run.py
@@ -1,14 +1,13 @@
-# import random function for game
+import random
 # ask for name
 board = [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ']
 
 
 def startGame():
-    print(f'welcome to TICTACTOE!\n')
-    name = input("Please enter your name: ")
     print(f'Thanks, {name}\n')
     print(f'{name} [X] --- Computer [O]\n')
     drawBoard()
+    randomFirstPlayer()
 
 
 def drawBoard():
@@ -16,17 +15,26 @@ def drawBoard():
     print("--+--+--")
     print(f' {board[3]}| {board[4]}| {board[5]}')
     print("--+--+--")
-    print(f' {board[6]}| {board[7]}| {board[8]}')
+    print(f' {board[6]}| {board[7]}| {board[8]}\n')
 
 
+def randomFirstPlayer():
+    firstPlayer = random.randint(1, 2)
+    if firstPlayer == 1:
+        print(f'{name} will go first')
+    elif firstPlayer == 2:
+        print('Computer will go first')
 
 # def for deciding who goes first
 # ask for position (add exceptions for invalid input)
 # draw computor position
-# def for drawing grid
 # add elif for win conditions
 # add def to check if its a draw if board is full
 # declare winner and ask if they wany to play again
+
+
+print('welcome to TICTACTOE!\n')
+name = input("Please enter your name: ")
 
 
 startGame()
