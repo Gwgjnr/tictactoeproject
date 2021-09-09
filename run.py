473d45fdffbfa0099f113e7b0dfd2a5525a1a714
Gwgjnr <geoffwgg@gmail.com>
2021-09-09 19:45:58 +0000

    added initial rules for win conditions

--- run.py
+++ run.py
@@ -3,6 +3,7 @@ import time
 import sys
 
 board = [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ']
+winner = False
 
 
 def startGame():
@@ -47,6 +48,30 @@ def computerTurn():
     board[selection] = "O"
     drawBoard()
     playerTurn()
+
+
+def checkWin():
+    # Horizontal Wins
+    if(board[1] == board[2] and board[2] == board[3] and board[1] != ' '):
+        winner = True
+    elif(board[4] == board[5] and board[5] == board[6] and board[4] != ' '):
+        winner = True
+    elif(board[7] == board[8] and board[8] == board[9] and board[7] != ' '):
+        winner = True
+    # Vertical Wins
+    elif(board[1] == board[4] and board[4] == board[7] and board[1] != ' '):
+        winner = True
+    elif(board[2] == board[5] and board[5] == board[8] and board[2] != ' '):
+        winner = True
+    elif(board[3] == board[6] and board[6] == board[9] and board[3] != ' '):
+        winner = True
+    # Diagonal Wins
+    elif(board[1] == board[5] and board[5] == board[9] and board[5] != ' '):
+        winner = True
+    elif(board[3] == board[5] and board[5] == board[7] and board[5] != ' '):
+        winner = True
+    else:
+        winner = False
 
 
 def print_slow(str):
